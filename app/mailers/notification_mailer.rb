e895cad6e3059a19e4a9c327594d0aa952056410
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2016-06-19 02:33:00 +0000

    Send email on comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,15 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomster-chuong-nguyen.herokuapp.com"
 
-  def comment_added
-    mail(to: 'chuong.v.nguyen@gmail.com',
-    subject: 'A comment has been added to your place')
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    if @place_owner
+      mail(to: @place_owner.email,
+           subject: "A comment has been added to #{@place.name}")
+     else
+       puts "No owner info found. Don't send notification."
+     end
   end
 end
